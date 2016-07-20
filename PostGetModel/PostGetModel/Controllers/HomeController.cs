bad0bb08b6e15ba06262f0c4700fbd63f22b7068
Thiago Henrique <thiagohensou@hotmail.com>
2016-07-20 01:12:15 -0300

    Primeiro commit

---
+++ PostGetModel/PostGetModel/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using PostGetModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PostGetModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa
+            {
+                PessoaId = 1,
+                Nome = "Thiago Henrique",
+                Twitter = "Não tenho"
+            };
+            //ViewBag.PessoaID = pessoa.PessoaId;
+            //ViewBag.Nome = pessoa.Nome;
+            //ViewData["Twitter"] = pessoa.Twitter;
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public ActionResult Lista(Pessoa pessoa)
+        {
+           
+
+            return View(pessoa);
+        }
+
+    }
+}
